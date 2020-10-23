e013f0361ed0e3db382f485ef7dcdc3d8ee8b691
Clinton Hipple <clintondhipple@hotmail.com>
2020-10-23 16:31:02 -0500

    file structure complete starting up server

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {});
