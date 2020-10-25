1eb9cb45f9849a20fc4572f4477c3b7552c79f67
Clinton Hipple <clintondhipple@hotmail.com>
2020-10-25 17:12:07 -0500

    working on put for time

---
+++ server/routes/time.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const config = {
+  database: 'weekend-to-do-app',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 10000,
+};
+
+const pool = new pg.Pool(config);
+
+router.put('/time/:id', (req, res) => {
+  const obj = req.body;
+  console.log(obj);
+  const queryText = `UPDATE "tasks" SET time=$1 WHERE id=$2;`;
+  const queryArray = [obj.time, req.params.id];
+
+  pool
+    .query(queryText, queryArray)
+    .then((dbResponse) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
